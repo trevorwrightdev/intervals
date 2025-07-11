e697a12334751be3e95e5f8a8c50357d3b220cb5
trevorwrightdev <trevor.wright.710@gmail.com>
2025-07-11 14:03:53 -0700

    set up dashboard page

---
+++ next-app/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function DashboardPage() {
+    const supabase = await createClient()
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+        redirect('/signin')
+    }
+    return (
+        <div className='flex-1 w-full flex flex-col items-center justify-center'>
+            <h1 className='font-bold text-3xl'>dashboard</h1>
+        </div>
+    )
+}
